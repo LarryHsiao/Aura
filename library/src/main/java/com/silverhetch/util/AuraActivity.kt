e229be8eb9e2c61c0e8b6391f017e1011507310e
LarryHsiao <larryhsiao@silverhetch.com>
2018-05-29 13:45:09 +0800

    AuraActivity as abstract

--- library/src/main/java/com/silverhetch/util/AuraActivity.kt
+++ library/src/main/java/com/silverhetch/util/AuraActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 /**
  * Aura style Activity. Inherit this class for all Aura feature.
  */
-class AuraActivity : AppCompatActivity() {
+abstract class AuraActivity : AppCompatActivity() {
     override fun onBackPressed() {
         supportFragmentManager.fragments.forEach {
             // NOTE: for now don`t care the priority of fragments.
