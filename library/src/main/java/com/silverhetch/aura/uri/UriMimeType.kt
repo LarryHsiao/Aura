f25bd1080b498838f8406aaa8b83179293407851
LarryHsiao <larryhsiao@larryhsiao.com>
2019-07-29 15:46:50 +0800

    UriMimeType

---
+++ library/src/main/java/com/silverhetch/aura/uri/UriMimeType.kt
@@ -0,0 +1,33 @@
+package com.silverhetch.aura.uri
+
+import android.webkit.MimeTypeMap
+import com.silverhetch.clotho.Source
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+
+
+/**
+ * Source to generate mime type from uri.
+ *
+ * This will be empty string if no match found.
+ */
+class UriMimeType(
+    private val context: Context,
+    private val url: String
+) : Source<String> {
+    companion object{
+        private const val UNRECOGNIZED_MIME_TYPE = ""
+    }
+
+    override fun value(): String {
+        return if (url.startsWith(ContentResolver.SCHEME_CONTENT)) {
+                val cr = context.contentResolver
+                cr.getType(Uri.parse(url)) ?: UNRECOGNIZED_MIME_TYPE
+            } else {
+                MimeTypeMap.getSingleton().getMimeTypeFromExtension(
+                    MimeTypeMap.getFileExtensionFromUrl(url).toLowerCase()
+                ) ?: UNRECOGNIZED_MIME_TYPE
+            }
+    }
+}
