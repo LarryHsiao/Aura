f674fdb718f0b163dbde1ca6c47aaf439003d9d8
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-08-04 06:52:12 +0800

    demo video url for mediaplayer demo

--- aurademo/src/main/java/com/silverhetch/aurademo/MainActivity.kt
+++ aurademo/src/main/java/com/silverhetch/aurademo/MainActivity.kt
@@ -118,7 +118,7 @@ class MainActivity : AppCompatActivity(), PermissionCallback {
                 }
                 8 -> {
                     startActivity(Intent(view.context, MediaPlayerDemoActivity::class.java).apply{
-                        data = Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+                        data = Uri.parse("https://larryhsiao.com:13000/sample.mkv")
                     })
                 }
                 9 -> {
