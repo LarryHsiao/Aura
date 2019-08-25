fd6d8f7e5dc6c19f0a9ec62f845d507ba4574644
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-08-25 13:13:18 +0800

    PickerIntent

---
+++ library/src/main/java/com/silverhetch/aura/intent/PickerIntent.kt
@@ -0,0 +1,42 @@
+package com.silverhetch.aura.intent
+
+import android.content.Intent
+import android.content.Intent.*
+import android.os.Build
+import android.os.Build.VERSION.*
+import android.os.Build.VERSION_CODES.*
+import com.silverhetch.clotho.Source
+
+/**
+ * Source to generate [Intent] for picking files in given MimeType.
+ *
+ * ```kotlin
+ * startActivity(
+ *    PickerIntent(
+ *      "image/ *",
+ *      "audio/ *"
+ *    ).value()
+ * )
+ * ```
+ *
+ * Note that the there is no guarantee that the launched app supports all the mimeType
+ * we provided but the first provided is guarantee.
+ */
+class PickerIntent(
+    private val mimeType: String,
+    private vararg val mimeTypes: String
+) : Source<Intent> {
+    override fun value(): Intent {
+        return Intent(ACTION_GET_CONTENT).also {
+            it.type = mimeType
+
+            if (SDK_INT > KITKAT) {
+                it.putExtra(EXTRA_MIME_TYPES,
+                    mimeTypes.toMutableList().apply {
+                        add(mimeType)
+                    }.toTypedArray()
+                )
+            }
+        }
+    }
+}
