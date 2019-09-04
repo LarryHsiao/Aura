7f13baf030f0f5fe250c4592b8e4e3d29327f119
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-09-04 17:04:13 +0800

    BundleString

---
+++ library/src/main/java/com/silverhetch/aura/bundle/BundleString.java
@@ -0,0 +1,73 @@
+package com.silverhetch.aura.bundle;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+import com.silverhetch.clotho.Source;
+
+import java.util.Arrays;
+
+/**
+ * String of a bundle`s properties have.
+ */
+public class BundleString implements Source<String> {
+    private final Bundle bundle;
+
+    public BundleString(Bundle bundle) {
+        this.bundle = bundle;
+    }
+
+    @Override
+    public String value() {
+        return toString(bundle);
+    }
+
+    private String toString(Bundle bundle) {
+        StringBuilder out = new StringBuilder("Bundle[");
+
+        if (bundle == null) {
+            out.append("null");
+        } else {
+            boolean first = true;
+            for (String key : bundle.keySet()) {
+                if (!first) {
+                    out.append(", ");
+                }
+
+                out.append(key).append('=');
+
+                Object value = bundle.get(key);
+
+                if (value instanceof int[]) {
+                    out.append(Arrays.toString((int[]) value));
+                } else if (value instanceof byte[]) {
+                    out.append(Arrays.toString((byte[]) value));
+                } else if (value instanceof boolean[]) {
+                    out.append(Arrays.toString((boolean[]) value));
+                } else if (value instanceof short[]) {
+                    out.append(Arrays.toString((short[]) value));
+                } else if (value instanceof long[]) {
+                    out.append(Arrays.toString((long[]) value));
+                } else if (value instanceof float[]) {
+                    out.append(Arrays.toString((float[]) value));
+                } else if (value instanceof double[]) {
+                    out.append(Arrays.toString((double[]) value));
+                } else if (value instanceof String[]) {
+                    out.append(Arrays.toString((String[]) value));
+                } else if (value instanceof CharSequence[]) {
+                    out.append(Arrays.toString((CharSequence[]) value));
+                } else if (value instanceof Parcelable[]) {
+                    out.append(Arrays.toString((Parcelable[]) value));
+                } else if (value instanceof Bundle) {
+                    out.append(toString((Bundle) value));
+                } else {
+                    out.append(value);
+                }
+
+                first = false;
+            }
+        }
+
+        out.append("]");
+        return out.toString();
+    }
+}
