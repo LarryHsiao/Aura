a5fed80e9838126b3472d7df1f4890adf7b82708
LarryHsiao <larryhsiao@larryhsiao.com>
2019-09-28 00:30:52 +0800

    ktlint

--- library/src/main/java/com/silverhetch/aura/AuraFragment.kt
+++ library/src/main/java/com/silverhetch/aura/AuraFragment.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.silverhetch.aura.permission.PermissionCallback
 import com.silverhetch.aura.permission.Permissions
