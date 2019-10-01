3a0c8c962996c69c2df286447330dc36b5a79334
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-10-01 12:02:23 +0800

    ktlint

--- library/src/main/java/com/silverhetch/aura/view/bitmap/CircledDrawable.kt
+++ library/src/main/java/com/silverhetch/aura/view/bitmap/CircledDrawable.kt
@@ -2,15 +2,9 @@ package com.silverhetch.aura.view.bitmap
 
 import android.content.res.Resources
 import android.graphics.Bitmap
-import android.graphics.Bitmap.Config.ARGB_8888
-import android.graphics.Color
 import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.silverhetch.clotho.Source
-import com.silverhetch.clotho.source.ConstSource
-import java.lang.Math.pow
-import kotlin.math.pow
-import kotlin.math.sqrt
 
 /**
  * Source to build a Drawable that crops the given bitmap to circle one.
