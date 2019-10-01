2abae3c76e05fc228572d0a3bffb3c9aa82ef7aa
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-10-01 11:28:54 +0800

    move out the padding drawing from Circled Drawable

--- library/src/main/java/com/silverhetch/aura/view/bitmap/CircledDrawable.kt
+++ library/src/main/java/com/silverhetch/aura/view/bitmap/CircledDrawable.kt
@@ -13,36 +13,18 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 /**
- * Source to build a Drawable that scale the given Bitmap to circled Bitmap that
- * enlarge the original [Bitmap] with padding to not cut the corners of original one.
+ * Source to build a Drawable that crops the given bitmap to circle one.
  *
  * Circle implemented with [RoundedBitmapDrawableFactory] which is in androidx.
  */
 class CircledDrawable(
     private val resources: Resources,
-    private val bitmap: Bitmap
+    private val bitmap: Source<Bitmap>
 ) : Source<Drawable> {
     override fun value(): Drawable {
-        val enlargedSize = sqrt(if (bitmap.width > bitmap.height) {
-            bitmap.width
-        } else {
-            bitmap.height
-        }.toDouble().pow(2.0) * 2).toInt()
         return RoundedBitmapDrawableFactory.create(
             resources,
-            MergedImage(
-                ConstSource(Bitmap.createBitmap(
-                    enlargedSize,
-                    enlargedSize,
-                    ARGB_8888
-                ).apply {
-                    setHasAlpha(true)
-                    eraseColor(Color.RED)
-                }),
-                ConstSource(bitmap),
-                (enlargedSize - bitmap.width) / 2,
-                (enlargedSize - bitmap.height) / 2
-            ).value()
+            bitmap.value()
         ).let { drawable ->
             drawable.isCircular = true
             drawable
