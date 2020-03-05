13e01761ce9ddc01b61c2c12be98fca38fbc7b12
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-03-05 12:45:48 +0800

    Empty List

---
+++ aurademo/src/main/java/com/silverhetch/aurademo/EmptyListAdapterActivity.kt
@@ -0,0 +1,47 @@
+package com.silverhetch.aurademo
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.silverhetch.aura.view.EmptyListAdapter
+import com.silverhetch.aura.view.ViewHolder
+
+/**
+ * Activity to demo the adapter of empty list
+ */
+class EmptyListAdapterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val listView = RecyclerView(this)
+        listView.layoutManager = LinearLayoutManager(this)
+        listView.adapter = EmptyListAdapter(object : RecyclerView.Adapter<ViewHolder>() {
+            override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+            ): ViewHolder {
+                return ViewHolder(View(parent.context))
+            }
+
+            override fun getItemCount(): Int {
+                return 0
+            }
+
+            override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            }
+        }, TextView(this).apply {
+            text = "Empty View"
+            setCompoundDrawablesWithIntrinsicBounds(
+                null,
+                resources.getDrawable(R.drawable.ic_cross),
+                null, null
+            )
+            gravity = Gravity.CENTER
+        })
+        setContentView(listView)
+    }
+}
