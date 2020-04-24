eda8daaa736a45979db38ccebdaf183f9d921bec
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-04-24 13:45:19 +0800

    Jpeg Compress

---
+++ library/src/test/java/com/silverhetch/aura/images/exif/ExifTagsTest.java
@@ -0,0 +1,32 @@
+package com.silverhetch.aura.images.exif;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.List;
+
+import static androidx.exifinterface.media.ExifInterface.*;
+
+/**
+ * Unit-test for the class {@link ExifTags}
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ExifTagsTest {
+
+    /**
+     * Check tags that is common to us.
+     */
+    @Test
+    public void checkCommonTags() {
+        final List<String> tags = new ExifTags().value();
+        Assert.assertTrue(tags.contains(TAG_GPS_LONGITUDE));
+        Assert.assertTrue(tags.contains(TAG_GPS_LATITUDE));
+        Assert.assertTrue(tags.contains(TAG_ORIENTATION));
+        Assert.assertTrue(tags.contains(TAG_COMPRESSION));
+        Assert.assertTrue(tags.contains(TAG_COPYRIGHT));
+        Assert.assertTrue(tags.contains(TAG_BITS_PER_SAMPLE));
+        Assert.assertFalse(tags.contains(null));
+    }
+}
