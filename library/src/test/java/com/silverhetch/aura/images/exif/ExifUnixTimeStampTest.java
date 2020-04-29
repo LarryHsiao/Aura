cd31912cbb7edb64db4ffe61daf7e075fd9309ca
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-04-29 10:58:15 +0800

    Exif UnitTimeStamp

---
+++ library/src/test/java/com/silverhetch/aura/images/exif/ExifUnixTimeStampTest.java
@@ -0,0 +1,40 @@
+package com.silverhetch.aura.images.exif;
+
+import androidx.exifinterface.media.ExifInterface;
+import com.silverhetch.clotho.source.ConstSource;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.io.File;
+
+import static androidx.exifinterface.media.ExifInterface.TAG_DATETIME_ORIGINAL;
+
+/**
+ * Unit-test for the class {@link ExifUnixTimeStamp}
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ExifUnixTimeStampTest {
+    /**
+     * Simple check
+     */
+    @Test
+    public void checkTime() throws Exception {
+        Assert.assertEquals(
+            new Long(1224694807000L), //2008:10:22 17:00:07
+            new ExifUnixTimeStamp(
+                new ExifAttribute(
+                    new ConstSource<>(
+                        new ExifInterface(
+                            new File(
+                                getClass().getClassLoader().getResource(
+                                    "exif_sample_geo_001.jpg"
+                                ).toURI()
+                            ).getAbsolutePath()
+                        )
+                    ), TAG_DATETIME_ORIGINAL
+                )
+            ).value());
+    }
+}
